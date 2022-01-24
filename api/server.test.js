d4e1445d7b5a6ec1bbb439c98670a5214e126b4c
Jacob Brooks <myrefugemyfortress@yahoo.com>
2022-01-24 01:57:53 -0500

    started tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,32 @@
+const model = require("./jokes/model.js")
+const db = require("../data/dbConfig.js")
+
+const dummy = {name:"test"}
+
+beforeAll(async ()=>{
+  await db.migrate.rollback();
+  await db.migrate.latest()
+})
+beforeEach(async () =>{
+  await db("users").truncate();
+})
+afterAll(async ()=>{
+  await db.destroy()
+})
+
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+describe("Model Tests",()=>{
+  describe("Test One",=>{
+    it("add user to database",async ()=>{
+      let all
+      await model.insert(dummy)
+      all = await db("users")
+      expect(all).toHaveLength(1)
+    })
+  })
 })
